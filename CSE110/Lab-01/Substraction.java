76e8892518cdac89fce6b9c998b148ae673cb876
Farhan Hasin Adib <farhanhasin20@gmail.com>
2025-05-25 19:50:42 +0600

    Add files via upload

---
+++ CSE110/Lab-01/Substraction.java
@@ -0,0 +1,11 @@
+public class Substraction 
+{
+    public static void main(String[] args)
+    {
+        int a = 8;
+        int b = 4;
+        int subs = a-b;
+        System.out.println(subs);
+    }
+    
+}
