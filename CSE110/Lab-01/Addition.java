a2103f523c27ec9c4f8176c3628e6f294017794c
Farhan Hasin Adib <farhanhasin20@gmail.com>
2025-05-25 19:32:26 +0600

    Initial Commit

--- CSE110/Lab-01/Addition.java
+++ CSE110/Lab-01/Addition.java
@@ -1,9 +1,9 @@
-public class Add
+public class Addition
 {
     public static void main(String[] args)
     {
-        int a = 6;
-        int b = 4;
+        int a = 5;
+        int b = 6;
         int sum = a+b;
         System.out.println(sum);
     }
