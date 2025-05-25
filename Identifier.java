8b971249d07be7ec188efeb0796b04fdc58f57f5
Farhan Hasin Adib <farhanhasin20@gmail.com>
2025-05-26 02:22:32 +0600

    Update and rename CSE110/Lab-01/LabAssignment(02)_24315032_FarhanHasin.txt.java to Identifier.java

---
+++ Identifier.java
@@ -0,0 +1,64 @@
+Task_Identifier
+  
+    (a) int hungry;     //Valid
+    (b) int 2AB;        //Invalid (Because it started with number)
+    (c) int 312.2;      //Invalid (Because it started with number and contains special character)
+    (d) int MOBILE;     //Valid
+    (e) int "Ans";      //Invalid (Because it contains special character)
+    (f) int $30;        //Valid
+    (g) int Yes/No;     //Invalid (Because Slash "/" is a Division operator in Java )
+    (h) int student-id; //Invalid (Because Hyphen "-" is a substraction operator in Java)
+    (i) int A+3;        //Invalid (Because Plus "+" is an Addition operator in Java)
+    (j) int 'X';        //Invalid (Because single quotes are used for characters)
+    (k) int return;     //Invalid (Because it is a reserve keyword)
+
+
+
+ 
+
+
+
+
+
+
+
+                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
